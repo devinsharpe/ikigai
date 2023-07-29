51c25b829c8e5500f911f7b8f137c4b165ec61aa
Devin Sharpe <devinrsharpe@gmail.com>
2023-07-29 19:21:54 -0400

    fix: add cn util to collapsible comp

--- src/components/collapsible.tsx
+++ src/components/collapsible.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import * as RadixCollapsible from '@radix-ui/react-collapsible';
 import { FoldVertical, X } from 'lucide-react';
+import { cn } from '~/lib/cn';
 
 export function CollapsibleActionButton(props: RadixCollapsible.PrimitiveButtonProps) {
-  return (<button className="p-2 rounded-lg hover:bg-zinc-100 flex items-center justify-center" {...props} />)
+  return (<button className={cn("p-2 rounded-lg hover:bg-zinc-100 flex items-center justify-center", props.className)} {...props} />)
 }
 
 interface CollapsibleItemProps {
   key: string;
   children: JSX.Element;
+  className?: string;
 }
 
-export function CollapsibleItem({ key, children }: CollapsibleItemProps) {
-  return <div className="p-3 flex items-center gap-2 rounded-lg bg-white border border-zinc-300 hover:bg-zinc-100 cursor-pointer animate-in fade-in" key={key} >
+export function CollapsibleItem({ key, children, ...props }: CollapsibleItemProps) {
+  return <div className={cn("p-3 flex items-center gap-2 rounded-lg bg-white border border-zinc-300 hover:bg-zinc-100 cursor-pointer animate-in fade-in", props.className)} key={key} >
     {children}
   </div>
 }
 
 interface CollapsibleProps {
   actions?: JSX.Element[];
+  className?: string;
   elements: JSX.Element[];
   isOpen: boolean;
   onOpenChange: (val: boolean) => void;
@@ -26,9 +29,9 @@ interface CollapsibleProps {
   title: string;
 }
 
-function Collapsible({ actions, elements, isOpen, onOpenChange, previewCount = 1, title }: CollapsibleProps) {
+function Collapsible({ actions, elements, isOpen, onOpenChange, previewCount = 1, title, ...props }: CollapsibleProps) {
   return (
-    <RadixCollapsible.Root className="flex flex-col gap-2" open={isOpen} onOpenChange={onOpenChange}>
+    <RadixCollapsible.Root className={cn("flex flex-col gap-2", props.className)} open={isOpen} onOpenChange={onOpenChange}>
       <div className="font-bold tracking-wide text-2xl text-zinc-800 flex items-center justify-between gap-2 px-2">
         <h3>{title}</h3>
         <div className="flex items-center gap-2">
