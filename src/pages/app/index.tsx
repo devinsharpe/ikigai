aaa1ec4f3746145dfa9d22b1393f7703969c5543
Devin Sharpe <devinrsharpe@gmail.com>
2023-08-03 00:03:59 -0400

    feat: add projects collapsible and modal logic

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,19 +1,71 @@
+import { useCallback, useEffect } from "react";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
-import { Home, LayoutDashboard, ListPlus, Plus, Search } from "lucide-react";
+import { Home, LayoutDashboard, Plus, Search } from "lucide-react";
 import { useState } from "react";
 import Collapsible, {
   CollapsibleActionButton,
   CollapsibleItem,
 } from "~/components/collapsible";
 import Modal from "~/components/modal";
+import { api } from "~/utils/api";
+import ProjectForm from "~/components/forms/project";
 
 function AppHomePage() {
   const [isNewProjectOpen, setIsNewProjectOpen] = useState(false);
   const [isProjectsOpen, setIsProjectsOpen] = useState(false);
-  const organization = useOrganization();
-  const organizationList = useOrganizationList();
-  console.log(organization);
-  console.log(organizationList);
+  const [newProjectDetails, setNewProjectDetails] = useState({
+    name: "",
+    description: "",
+    organization: "",
+  });
+  const currentOrganization = useOrganization();
+  const { organizationList, setActive } = useOrganizationList();
+  const projects = api.projects.list.useQuery();
+  const createProject = api.projects.create.useMutation();
+
+  const handleNewProject = useCallback(
+    async (project: {
+      name: string;
+      description: string;
+      organization: string;
+    }) => {
+      if (project.name) {
+        if (setActive) await setActive({ organization: project.organization });
+        const newProject = await createProject.mutateAsync({
+          name: project.name,
+          description: project.description,
+        });
+        console.log(newProject);
+        void projects.refetch();
+        setIsNewProjectOpen(false);
+        setNewProjectDetails({
+          name: "",
+          description: "",
+          organization: "",
+        });
+      } else {
+        console.log("missing name");
+      }
+    },
+    [
+      projects,
+      setActive,
+      createProject,
+      setIsNewProjectOpen,
+      setNewProjectDetails,
+    ]
+  );
+
+  useEffect(() => {
+    void projects.refetch();
+    setNewProjectDetails((n) => ({
+      ...n,
+      organization: currentOrganization.organization
+        ? currentOrganization.organization.id
+        : "",
+    }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentOrganization.organization]);
 
   return (
     <>
@@ -46,15 +98,16 @@ function AppHomePage() {
         </div>
       </form>
       <main className="container mx-auto flex min-h-screen flex-col flex-col gap-4 px-2 pt-20">
-        <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-2 lg:grid-cols-2">
           <Collapsible
+            isLoading={projects.isLoading}
             isOpen={isProjectsOpen}
             onOpenChange={() => setIsProjectsOpen(!isProjectsOpen)}
-            previewCount={2}
+            previewCount={3}
             actions={[
               <CollapsibleActionButton
-                title="New Project"
                 key="project-new"
+                title="New Project"
                 onClick={() => setIsNewProjectOpen(true)}
               >
                 <>
@@ -63,76 +116,41 @@ function AppHomePage() {
                 </>
               </CollapsibleActionButton>,
             ]}
-            elements={[
-              <CollapsibleItem key="0">
-                <>
-                  <span>🎯</span>
-                  <span className="font-semibold">DevSharpe</span>
-                  <span className="ml-auto rounded-lg bg-zinc-200 px-3 py-1 text-sm">
-                    7
-                  </span>
-                </>
-              </CollapsibleItem>,
-              <CollapsibleItem key="1">
-                <>
-                  <span>📒</span>
-                  <span className="font-semibold">Day Job</span>
-                  <span className="ml-auto rounded-lg bg-zinc-200 px-3 py-1 text-sm">
-                    11
-                  </span>
-                </>
-              </CollapsibleItem>,
-              <CollapsibleItem key="2">
-                <>
-                  <span>❄️</span>
-                  <span className="font-semibold">Personal</span>
-                  <span className="ml-auto rounded-lg bg-zinc-200 px-3 py-1 text-sm">
-                    3
-                  </span>
-                </>
-              </CollapsibleItem>,
-            ]}
+            elements={
+              projects.data
+                ? projects.data.map((project) => (
+                    <CollapsibleItem key={project.id}>
+                      <>
+                        <span className="font-semibold">{project.name}</span>
+                        <span className="ml-auto rounded-lg bg-zinc-200 px-3 py-1 text-sm">
+                          7
+                        </span>
+                      </>
+                    </CollapsibleItem>
+                  ))
+                : []
+            }
             title="Projects"
           />
         </div>
       </main>
       <Modal
-        isOpen={isNewProjectOpen}
+        isOpen={createProject.isLoading || isNewProjectOpen}
         onOpenChange={() => setIsNewProjectOpen(!isNewProjectOpen)}
         title="New Project"
         description="Building a rocketship? Starting a new business? Renovating a home? Keeping up with your meds?"
       >
-        <form className="flex flex-col gap-2">
-          <input
-            className="w-full rounded-lg border border-zinc-300 p-3 text-zinc-800"
-            placeholder="Name your project"
-            title="Project Name"
-          />
-          <textarea
-            className="h-32 w-full rounded-lg border border-zinc-300 p-3 text-zinc-800"
-            placeholder="Describe your project"
-            title="Project Description"
-          />
-          <select
-            title="Project Organization"
-            className="w-full rounded-lg border border-zinc-300 p-3 text-zinc-800"
-          >
-            <option value="">Personal Workspace</option>
-            {organizationList.organizationList ? (
-              organizationList.organizationList.map((org) => (
-                <option value={org.organization.id} key={org.organization.id}>
-                  {org.organization.name}
-                </option>
-              ))
-            ) : (
-              <></>
-            )}
-          </select>
-          <button className="flex w-full items-center justify-center gap-2 rounded-lg bg-zinc-800 p-3 text-white text-zinc-800 hover:bg-zinc-900">
-            <ListPlus />
-            <span>Create Project</span>
-          </button>
-        </form>
+        <ProjectForm
+          isLoading={createProject.isLoading}
+          organizations={
+            organizationList
+              ? organizationList.map((org) => org.organization)
+              : []
+          }
+          project={newProjectDetails}
+          onChange={setNewProjectDetails}
+          onSubmit={handleNewProject}
+        />
       </Modal>
     </>
   );
