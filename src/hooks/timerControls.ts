6508faaca172a22c4b0800e6149b0d435d9b78e9
Devin Sharpe <devinrsharpe@gmail.com>
2023-08-14 21:28:56 -0400

    feat: add timercontrols hook

---
+++ src/hooks/timerControls.ts
@@ -0,0 +1,98 @@
+import { useCallback, useEffect, useState } from "react";
+import { api } from "~/utils/api";
+import type { SimpleTimer } from "~/components/forms";
+import { toTZISOString } from "~/lib/date";
+
+const initialTimerData = {
+  name: "",
+  description: "",
+  organization: "",
+  projectId: "",
+  startedAt: "",
+  stoppedAt: "",
+};
+
+export function useTimerControls() {
+  const [isTimerModalOpen, setIsTimerModalOpen] = useState(false);
+  const [timerDetails, setTimerDetails] = useState<
+    SimpleTimer & { id?: string }
+  >(initialTimerData);
+  const timers = api.timers.list.useQuery();
+  const createTimer = api.timers.create.useMutation();
+  const updateTimer = api.timers.update.useMutation();
+  const deleteTimer = api.timers.delete.useMutation();
+  const stopTimer = api.timers.stop.useMutation();
+
+  useEffect(() => {
+    if (!isTimerModalOpen) setTimerDetails(initialTimerData);
+  }, [isTimerModalOpen, setTimerDetails]);
+
+  const handleTimerSubmit = useCallback(
+    async (timer: SimpleTimer & { id?: string }) => {
+      if (timer.name && timer.projectId && timer.startedAt) {
+        if (timer.id)
+          await updateTimer.mutateAsync({
+            ...timer,
+            id: timer.id,
+            description: timer.description ?? "",
+            startedAt: timer.startedAt
+              ? toTZISOString(new Date(timer.startedAt))
+              : undefined,
+            stoppedAt: timer.stoppedAt
+              ? toTZISOString(new Date(timer.stoppedAt))
+              : undefined,
+          });
+        else
+          await createTimer.mutateAsync({
+            ...timer,
+            description: timer.description ?? "",
+            startedAt: timer.startedAt
+              ? toTZISOString(new Date(timer.startedAt))
+              : undefined,
+            stoppedAt: timer.stoppedAt
+              ? toTZISOString(new Date(timer.stoppedAt))
+              : undefined,
+          });
+        void timers.refetch();
+        setIsTimerModalOpen(false);
+        setTimerDetails(initialTimerData);
+      } else if (!timer.name) {
+        console.log("missing name");
+      } else if (!timer.projectId) {
+        console.log("missing project");
+      }
+    },
+    [timers, createTimer, updateTimer, setIsTimerModalOpen, setTimerDetails]
+  );
+
+  const handleStopTimer = useCallback(
+    async (id: string) => {
+      await stopTimer.mutateAsync({ id });
+      void timers.refetch();
+    },
+    [timers, stopTimer]
+  );
+
+  const handleDeleteTimer = useCallback(
+    async (id: string) => {
+      await deleteTimer.mutateAsync({ id });
+      void timers.refetch();
+    },
+    [timers, deleteTimer]
+  );
+
+  return {
+    isTimerModalOpen,
+    setIsTimerModalOpen,
+    timers,
+    timerDetails,
+    setTimerDetails,
+    createTimer,
+    deleteTimer,
+    updateTimer,
+    stopTimer,
+    handleDeleteTimer,
+    handleTimerSubmit,
+    handleStopTimer,
+  };
+}
