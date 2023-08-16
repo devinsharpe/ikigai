5f411eee06fa0d15515674a6fc0d42d6cf1ee164
Devin Sharpe <devinrsharpe@gmail.com>
2023-08-16 00:40:53 -0400

    fix: add onclick prop to dropdown

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,33 +1,57 @@
 import * as RadixDropdown from "@radix-ui/react-dropdown-menu";
+import { cn } from "~/lib/cn";
 
 interface DropdownItemProps {
   children: JSX.Element;
+  className?: string;
+  onClick?: () => void;
 }
 
-export function DropdownItem({ children }: DropdownItemProps) {
+export function DropdownItem({
+  children,
+  className,
+  onClick,
+}: DropdownItemProps) {
   return (
     <RadixDropdown.Item
-      className="flex w-full items-center gap-2 rounded-md p-2 hover:bg-zinc-200 focus:outline-none"
-      asChild
+      className={cn(
+        "flex w-full cursor-pointer items-center gap-2  rounded p-2 text-sm font-medium hover:bg-zinc-100 focus:outline-none md:text-base",
+        className
+      )}
+      onClick={onClick}
     >
       {children}
     </RadixDropdown.Item>
   );
 }
 
+export function DropdownSeparator({ className }: { className?: string }) {
+  return (
+    <RadixDropdown.Separator
+      className={cn("m-1 h-[1px] bg-zinc-300", className)}
+    />
+  );
+}
+
 interface DropdownProps {
+  className?: string;
   children: JSX.Element;
   trigger: JSX.Element;
 }
 
-function Dropdown({ children, trigger }: DropdownProps) {
+function Dropdown({ children, className, trigger }: DropdownProps) {
   return (
     <RadixDropdown.Root>
       <RadixDropdown.Trigger asChild className="focus:outline-none">
         {trigger}
       </RadixDropdown.Trigger>
       <RadixDropdown.Portal>
-        <RadixDropdown.Content className="mr-4 min-w-[12rem] rounded-lg border bg-white p-2 shadow-lg animate-in fade-in">
+        <RadixDropdown.Content
+          className={cn(
+            "mr-4 flex min-w-[14rem] flex-col gap-1 rounded-lg border bg-white p-2 shadow-lg animate-in fade-in md:min-w-[16rem]",
+            className
+          )}
+        >
           <>{children}</>
         </RadixDropdown.Content>
       </RadixDropdown.Portal>
