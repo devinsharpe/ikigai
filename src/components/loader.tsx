80f413779415677104e2a16db9c7071e3678de43
Devin Sharpe <devinrsharpe@gmail.com>
2023-08-30 05:52:23 -0400

    feat: add loader component

---
+++ src/components/loader.tsx
@@ -0,0 +1,15 @@
+import { Loader2 } from "lucide-react";
+import { cn } from "~/lib/cn";
+
+interface LoaderProps {
+  className?: string;
+  children: JSX.Element;
+  isLoading: boolean;
+}
+
+function Loader({ className, children, isLoading }: LoaderProps) {
+  if (isLoading) return <Loader2 className={cn("animate-spin", className)} />;
+  return children;
+}
+
+export default Loader;
