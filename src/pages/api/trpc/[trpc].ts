a056b3465dcb6c984b60b843cb8c3ee199a1eb0f
Devin Sharpe <devinrsharpe@gmail.com>
2023-08-31 01:40:03 -0400

    feat: edge trpc?

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,18 +1,48 @@
-import { createNextApiHandler } from "@trpc/server/adapters/next";
+// import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { getAuth } from "@clerk/nextjs/server";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import type { NextRequest } from "next/server";
 import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
-import { createTRPCContext } from "~/server/api/trpc";
+import { createInnerTRPCContext } from "~/server/api/trpc";
+
+export default async function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    router: appRouter,
+    req,
+    createContext() {
+      const auth = getAuth(req);
+      return createInnerTRPCContext({
+        auth,
+        req,
+      });
+    },
+    onError:
+      env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            );
+          }
+        : undefined,
+  });
+}
+
+export const config = {
+  runtime: "edge",
+};
 
 // export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
-          );
-        }
-      : undefined,
-});
+// export default createNextApiHandler({
+//   router: appRouter,
+//   createContext: createTRPCContext,
+// onError:
+//   env.NODE_ENV === "development"
+//     ? ({ path, error }) => {
+//         console.error(
+//           `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+//         );
+//       }
+//     : undefined,
+// });
