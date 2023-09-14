f0941197dbac20aa1411783d459591e72bd33ab5
Devin Sharpe <devinrsharpe@gmail.com>
2023-09-14 01:32:20 -0400

    feat: add project router endpoints

--- src/server/api/routers/projects.ts
+++ src/server/api/routers/projects.ts
@@ -3,6 +3,7 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { and, asc, eq, isNull } from "drizzle-orm";
 import { projects } from "~/server/db/schema";
 import { createId } from "~/server/db/utils";
+import { TRPCError } from "@trpc/server";
 
 export const projectsRouter = createTRPCRouter({
   create: protectedProcedure
@@ -24,6 +25,48 @@ export const projectsRouter = createTRPCRouter({
         })
         .returning();
       return newProject;
+    }),
+  delete: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const project = await ctx.db.query.projects.findFirst({
+        where: and(
+          eq(projects.id, input.id),
+          ...(ctx.auth.orgId
+            ? [eq(projects.organization, ctx.auth.orgId)]
+            : [
+                eq(projects.createdBy, ctx.auth.userId),
+                isNull(projects.organization),
+              ])
+        ),
+      });
+      if (!project)
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Project not found",
+        });
+      await ctx.db.delete(projects).where(eq(projects.id, project.id));
+      return project;
+    }),
+  get: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const project = await ctx.db.query.projects.findFirst({
+        where: and(
+          eq(projects.id, input.id),
+          ...(ctx.auth.orgId
+            ? [eq(projects.organization, ctx.auth.orgId)]
+            : [
+                eq(projects.createdBy, ctx.auth.userId),
+                isNull(projects.organization),
+              ])
+        ),
+      });
+      return project;
     }),
   list: protectedProcedure.query(async ({ ctx }) => {
     const projectList = await ctx.db.query.projects.findMany({
