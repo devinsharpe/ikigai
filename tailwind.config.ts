12300693c10e8168efcfa7feff12791381921db1
Devin Sharpe <devinrsharpe@gmail.com>
2023-09-21 01:51:04 -0400

    feat: add grid classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,14 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        "7": "repeat(7, minmax(0, 1fr))",
+        "8": "repeat(8, minmax(0, 1fr))",
+        "9": "repeat(9, minmax(0, 1fr))",
+        "10": "repeat(10, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/forms")],
 } satisfies Config;
