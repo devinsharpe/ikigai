f39962236a19ee4685eb308ca20220cc86181458
Devin Sharpe <devinrsharpe@gmail.com>
2023-09-21 01:51:36 -0400

    fix: remove console.log

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,7 +3,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "~/env.mjs";
 import schema from "./schema";
 
-console.log("DB_URL: ", env.DB_URL);
 const db = drizzle(neon(env.DB_URL), {
   schema,
 });
